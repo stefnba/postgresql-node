305938036d89145dead0b7a64c10c2e67f1cae3b
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-12-07 13:52:59 +0100

    feat: dotenv added

--- testing/index.test.ts
+++ testing/index.test.ts
@@ -1,16 +1,25 @@
 import chai from 'chai';
 import mocha from 'mocha';
+import dotenv from 'dotenv';
+
 import PostgresClient from '../src';
 
 const { it, describe } = mocha;
 const { expect } = chai;
+dotenv.config();
 
 const connection = {
-    user: 'admin',
-    host: 'localhost',
-    database: 'app_db',
-    password: 'password',
-    port: 5413
+    // user: 'admin',
+    // host: 'localhost',
+    // database: 'app_db',
+    // password: 'password',
+    // port: 5413
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    max: 30
 };
 
 let db: PostgresClient;
@@ -25,6 +34,14 @@ describe('PostgresClient', () => {
         it('SHOULD run test query', async () => {
             const r = await db.query.single<{ now: Date }>('SELECT NOW()');
             expect(r).to.have.property('now');
+        });
+        it('SHOULD return connection details', async () => {
+            const connection = db.connectionConfig;
+            expect(connection).to.have.property('host');
+            expect(connection).to.have.property('port');
+            expect(connection).to.have.property('database');
+            expect(connection).to.have.property('user');
+            expect(connection).to.have.property('password');
         });
     });
 });
