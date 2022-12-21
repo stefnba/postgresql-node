9697ac20cdedda6543f21a39ead9f01839a814bc
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-12-21 22:34:37 +0100

    fix: QueryError added

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export enum QueryErrorCodes {
+    NoUpdateColumns = 'NoUpdateColumns',
+    EmptyQuery = 'EmptyQuery',
+    MultipleRowsReturned = 'MultipleRowsReturned',
+    ExecutionError = 'ExecutionError'
+}
