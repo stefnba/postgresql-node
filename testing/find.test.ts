245408e0de6d08de7dc718a47c4f61efc6140608
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-01-15 20:08:43 +0100

    fix: testing

--- testing/find.test.ts
+++ testing/find.test.ts
@@ -22,7 +22,7 @@ const db = new PostgresClient(connection, {
 
 describe('FIND', () => {
     describe('MANY', () => {
-        it('SHOULD LIST all user records', async () => {
+        it('SHOULD FIND all user records', async () => {
             const r = await db.query
                 .find('SELECT * FROM Users')
                 .many<UserModel>();
@@ -30,7 +30,7 @@ describe('FIND', () => {
             expect(r).to.be.an('array');
             expect(r[0]).to.have.keys(['id', 'name', 'email', 'rank']);
         });
-        it('SHOULD LIST 15 user records', async () => {
+        it('SHOULD FIND 15 user records', async () => {
             const count = 15;
             const r = await db.query
                 .find('SELECT * FROM Users', {
@@ -44,17 +44,37 @@ describe('FIND', () => {
         });
     });
     describe('ONE', () => {
-        it('SHOULD LIST one user record', async () => {
+        it('SHOULD FIND one user record', async () => {
             const r = await db.query
                 .find('SELECT * FROM Users WHERE id = 1')
                 .one<UserModel>();
 
             expect(r).to.have.keys(['id', 'name', 'email', 'rank']);
         });
-        it('SHOULD LIST one user record with param id provided', async () => {
+        it('SHOULD FIND one user record with param id provided', async () => {
             const r = await db.query
                 .find('SELECT * FROM Users WHERE id = $<id>', {
                     params: { id: 1 }
+                })
+                .one<UserModel>();
+
+            expect(r).to.have.keys(['id', 'name', 'email', 'rank']);
+            expect(r.id).to.equal(1);
+        });
+        it('SHOULD FIND one user record with filter object', async () => {
+            const r = await db.query
+                .find('SELECT * FROM Users', {
+                    filter: { filter: { id: 1 }, filterSet: { id: 'EQUAL' } }
+                })
+                .one<UserModel>();
+
+            expect(r).to.have.keys(['id', 'name', 'email', 'rank']);
+            expect(r.id).to.equal(1);
+        });
+        it('SHOULD FIND one user record with filter string', async () => {
+            const r = await db.query
+                .find('SELECT * FROM Users', {
+                    filter: 'id = 1'
                 })
                 .one<UserModel>();
 
